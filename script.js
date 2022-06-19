63a1dc3bcbd7cd729f0f10009c37e608c4b02047
Chiamaka Ugwu <mannehills@gmail.com>
2022-06-19 18:42:15 +0100

    Update

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ menuIcon.addEventListener("click", function(){
     dropDown.style.display = "block";
     menuIcon.style.display = "none";
     close.style.display = "block";
-    close.style.zIndex = "900000000000000";
     blur.style.backgroundColor = "rgba(0, 0, 0, 0.3)";
     nav.style.backgroundColor = "rgba(0, 0, 0, 0.3)";
     nav.style.borderBottom = "1px solid rgba(0, 0, 0, 0.01)";
@@ -30,19 +29,19 @@ close.addEventListener("click", function(){
 })
 
 
-// const x = window.matchMedia("(max-width: 1000px)");
-// if(x.matches){
-//     document.onclick = function(e){
-//         if(e.target.id !== "dropdown" && e.target.id !== "menu-icon"){
-//             dropDown.style.display = "none";
-//             menuIcon.style.display = "block";
-//             close.style.display = "none"
-//             blur.style.backgroundColor = "transparent";
-//             nav.style.backgroundColor = "#fff";
-//             nav.style.borderBottom = "1px solid #BF9553";      
-//         }
-//     }
-// }
+const x = window.matchMedia("(max-width: 1000px)");
+if(x.matches){
+    document.onclick = function(e){
+        if(e.target.id !== "dropdown" && e.target.id !== "menu-icon" && e.target.id !== "dropdownMenuLink"){
+            dropDown.style.display = "none";
+            menuIcon.style.display = "block";
+            close.style.display = "none"
+            blur.style.backgroundColor = "transparent";
+            nav.style.backgroundColor = "#fff";
+            nav.style.borderBottom = "1px solid #BF9553";      
+        }
+    }
+}
 
 
 const active = window.matchMedia("(max-width: 2000px)");
