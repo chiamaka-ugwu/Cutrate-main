b6c2b7f6a12e3f5c9c941a82b3fe8dfc3416ab46
Chiamaka Mary-Anne Ugwu <mannehills@gmail.com>
2022-09-05 15:11:56 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -56,42 +56,64 @@ if(active.matches){
 }
 
 
-const togglePassword = document.querySelector('#togglePassword');
-const password = document.querySelector('#id_password');
- 
-togglePassword.addEventListener('click', function (e) {
-    // toggle the type attribute
-    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-    password.setAttribute('type', type);
-    // toggle the eye slash icon
-    this.classList.toggle('fa-eye-slash');
-});
-
-const togglePassword2 = document.querySelector('#togglePassword2');
-const password2 = document.querySelector('#id_password2');
- 
-togglePassword2.addEventListener('click', function (e) {
-    // toggle the type attribute
-    const type = password2.getAttribute('type') === 'password' ? 'text' : 'password';
-    password2.setAttribute('type', type);
-    // toggle the eye slash icon
-    this.classList.toggle('fa-eye-slash');
-});
+// const togglePassword = document.querySelector('#togglePassword');
+// const password = document.querySelector('#id_password');
+// togglePassword.addEventListener('click', function (e) {
+//     // toggle the type attribute
+//     const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+//     password.setAttribute('type', type);
+//     // toggle the eye slash icon
+//     this.classList.toggle('fa-eye-slash');
+// });
 
 
+// const box_one = document.getElementById("hide");
+// const box_two = document.getElementById("show");
+// let next = document.getElementsById("buttonX");
+// const back = document.getElementsById("back");
 
 
+// TOGGLE PASSWORD VISIBILITY
+const password = document.getElementById("password");
+const eye = document.getElementById("eye");
+const eyeSlash = document.getElementById("eye-slash");
 
-const box_one = document.getElementById("hide");
-const box_two = document.getElementById("show");
-let next = document.getElementsById("buttonX");
-const back = document.getElementsById("back");
-
-
-
-
-document.querySelectorAll(".image-container img").forEach(image => {
-    image.onclick = () => {
-
-    }
+eye.addEventListener("click", function(){
+    eye.style.display ="none";
+    eyeSlash.style.display = "block";
+    password.setAttribute('type', 'text');
 })
+eyeSlash.addEventListener("click", function(){
+    eye.style.display ="block";
+    eyeSlash.style.display = "none";
+    password.setAttribute('type', 'password');
+})
+
+
+const password2 = document.getElementById("password2");
+const eye2 = document.getElementById("eye2");
+const eyeSlash2 = document.getElementById("eye-slash2");
+
+eye2.addEventListener("click", function(){
+    eye2.style.display ="none";
+    eyeSlash2.style.display = "block";
+    password2.setAttribute('type', 'text');
+})
+eyeSlash2.addEventListener("click", function(){
+    eye2.style.display ="block";
+    eyeSlash2.style.display = "none";
+    password2.setAttribute('type', 'password');
+})
+
+const file = document.querySelector('#file');
+file.addEventListener('change', (e) => {
+  // Get the selected file
+  const [file] = e.target.files;
+  // Get the file name and size
+  const { name: fileName, size } = file;
+  // Convert size in bytes to kilo bytes
+  const fileSize = (size / 1000).toFixed(2);
+  // Set the text content
+  const fileNameAndSize = `${fileName} - ${fileSize}KB`;
+  document.querySelector('.file-name').textContent = fileNameAndSize;
+});
